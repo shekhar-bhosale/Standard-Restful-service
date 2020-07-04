ef2be2c2ae71f9a9115758a3994fcf33d8a81bde
Shekhar Bhosale <bhosale@adobe.com>
2020-07-04 14:03:27 -0600

    exception handling

---
+++ src/main/java/com/practice/restwebservices/restfulwebservices/exceptions/ExceptionResponse.java
@@ -0,0 +1,20 @@
+package com.practice.restwebservices.restfulwebservices.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+public class ExceptionResponse {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ExceptionResponse(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+}
